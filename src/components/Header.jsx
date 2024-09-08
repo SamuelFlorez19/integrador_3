8b259fbdf8669646927ecddf59b56527d0d65110
Samuel Florez <samuelflorezf@gmail.com>
2024-09-08 12:44:52 -0500

    Finaliza el NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,16 @@
 import React from 'react'
 import './Header.scss'
+import Navbar from './Navbar'
+import SearchBar from './SearchBar'
 
 const Header = () => {
     return (
         <header className="main-header">
             <input type="checkbox" id="menu" /> 
 
-            <nav className="nav-bar">
-                <ul className="nav-bar__nav-list">
-                <li className="nav-bar__nav-item">
-                    <a href="#" className="nav-bar__nav-link">Inicio</a>
-                </li>
-                <li className="nav-bar__nav-item">
-                    <a href="#" className="nav-bar__nav-link">Alta</a>
-                </li>
-                <li className="nav-bar__nav-item">
-                    <a href="views/nosotros.html" className="nav-bar__nav-link">Nosotros</a>
-                </li>
-                <li className="nav-bar__nav-item">
-                    <a href="views/contacto.html" className="nav-bar__nav-link">Contacto</a>
-                </li>
-                </ul>
-            </nav> 
-
-            <div className="search-bar">
-                <div className="search-bar__logo-container">
-                <h1 className="search-bar__logo">Crown Jewels</h1>
-                </div>
-                <form action="#" className="search-bar__form-container">
-                <label htmlFor="busqueda" className="search-bar__form-label">Buscar</label>
-                <input type="search" className="search-bar__form-search" id="busqueda"></input>
-                <button className="search-bar__button">Buscar</button>
-                </form>
-                <div className="search-bar__carrito-container"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg></div>
-
-                <div className="menu-toogle">
-                <label htmlFor="menu" className="menu-toogle__label">
-                    <span className="menu-toogle__top-bread"></span>
-                    <span className="menu-toogle__meat"></span>
-                    <span className="menu-toogle__bottom-bread"></span>
-                </label>
-                </div> 
-            </div>
+            <Navbar />
+            <SearchBar />
+            
         </header>
     )
 }
